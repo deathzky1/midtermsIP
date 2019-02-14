4eb40ae337733095eef64191e8df451c533f87dd
deathzky1 <khern02@yahoo.com>
2019-02-14 11:46:18 +0800

    Added monster and animation

---
+++ Draw.java
@@ -0,0 +1,238 @@
+import javax.swing.JComponent;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+public class Draw extends JComponent{
+
+	private BufferedImage image;
+	private BufferedImage backgroundImage;
+	public URL resource = getClass().getResource("run0.png");
+	private boolean idle = true;
+	private boolean alive = true;
+	// circle's position
+	public int x = 30;
+	public int y = 30;
+
+	// animation states
+	public int state = 0;
+
+	Monster monster1;
+	Monster monster2;
+	Monster monster3; //*added new Monster *//
+
+	public Draw(){
+		monster1 = new Monster(200, 200);
+		monster2 = new Monster(300, 200);
+		monster3 = new Monster(400, 200);
+
+		try{
+			image = ImageIO.read(resource);
+			backgroundImage = ImageIO.read(getClass().getResource("background.jpg"));
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void reloadImage(){
+		state++;
+
+		if(state == 0){
+			resource = getClass().getResource("run0.png");
+		}
+		else if(state == 1){
+			resource = getClass().getResource("run1.png");
+		}
+		else if(state == 2){
+			resource = getClass().getResource("run2.png");
+		}
+		else if(state == 3){
+			resource = getClass().getResource("run3.png");
+		}
+		else if(state == 4){
+			resource = getClass().getResource("run4.png");
+		}
+		else if(state == 5){
+			resource = getClass().getResource("run5.png");
+			state = 0;
+		}
+
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void attackAnimation(){
+		Thread thread1 = new Thread(new Runnable(){
+			public void run(){
+				for(int ctr = 0; ctr < 5; ctr++){
+					try {
+						if(ctr==4){
+							resource = getClass().getResource("idle/idle0.png");
+						}
+						else{
+							resource = getClass().getResource("attack"+ctr+".png");
+						}
+						
+						try{
+							image = ImageIO.read(resource);
+						}
+						catch(IOException e){
+							e.printStackTrace();
+						}
+				        repaint();
+				        Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread1.start();
+	}
+
+	public void airAnimation(){
+		Thread thread2 = new Thread(new Runnable(){
+			public void run(){
+				for(int ctr = 0; ctr < 14; ctr++){
+					try{
+						if(ctr == 13){
+							resource = getClass().getResource("idle/idle0.png");
+						}
+						else{
+							resource = getClass().getResource("airatt/air"+ctr+".png");
+						}
+						try{
+							image = ImageIO.read(resource);
+						}
+						catch(IOException e){
+							e.printStackTrace();
+						}
+						repaint();
+						Thread.sleep(100);
+					
+					}catch (InterruptedException e){
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread2.start();
+	}
+
+	public void bowAnimation(){
+		Thread thread3 = new Thread(new Runnable(){
+			public void run(){
+				for(int ctr = 0; ctr < 16; ctr++){
+					try{
+						if(ctr == 15){
+							resource = getClass().getResource("idle/idle0.png");
+						}
+						else{
+							resource = getClass().getResource("bowanim/bow"+ctr+".png");
+						}
+						try{
+							image = ImageIO.read(resource);
+						}
+						catch(IOException e){
+							e.printStackTrace();
+						}
+						repaint();
+						Thread.sleep(100);
+					
+					}catch (InterruptedException e){
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread3.start();
+	}
+
+	public void drawAnimation(){
+		Thread thread4 = new Thread(new Runnable(){
+			public void run(){
+				for(int ctr = 0; ctr < 9; ctr++){
+					try{
+						if(ctr == 8){
+							resource = getClass().getResource("idle/idle0.png");
+						}
+						else{
+							resource = getClass().getResource("sworddraw/draw"+ctr+".png");
+						}
+						try{
+							image = ImageIO.read(resource);
+						}
+						catch(IOException e){
+							e.printStackTrace();
+						}
+						repaint();
+						Thread.sleep(100);
+					
+					}catch (InterruptedException e){
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread4.start();
+	}
+	
+	public void attack(){
+		attackAnimation();
+	}
+
+	public void airAttack(){
+		airAnimation();
+	}
+
+	public void bowAttack(){
+		bowAnimation();
+	}
+	public void drawSword(){
+		drawAnimation();
+	}
+
+	public void moveUp(){
+		y = y - 5;
+		reloadImage();
+		repaint();
+	}
+
+	public void moveDown(){
+		y = y + 5;
+		reloadImage();
+		repaint();
+	}
+
+	public void moveLeft(){
+		x = x - 5;
+		reloadImage();
+		repaint();
+	}
+
+	public void moveRight(){
+		x = x + 5;
+		reloadImage();
+		repaint();
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.YELLOW);
+		g.drawImage(backgroundImage, 0, 0, this);
+		g.drawImage(image, x, y, this);
+
+		g.drawImage(monster1.image, monster1.xPos, monster1.yPos, this);
+		g.drawImage(monster2.image, monster2.xPos, monster2.yPos, this);
+		g.drawImage(monster3.image, monster3.xPos, monster2.yPos, this);
+	}
+}
